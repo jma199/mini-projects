354bb488fafaf2461e6ebc00cf7f18df6cdd4a08
Jennifer Ma <53239177+jma199@users.noreply.github.com>
2022-09-03 15:38:31 -0400

    Create 10 sukodu_solver.py
    initial commit of the sudoku solver

---
+++ 12 Beginner Python Projects freecodecamp/10 sudoku_solver.py
@@ -0,0 +1,15 @@
+'''
+A Sudoku solver that using recursion to solve the puzzle.
+This is an original implementation by K. Ying (Github User @kying18)
+'''
+
+def is_valid():
+    '''Determine whether a guess is valid'''
+    pass
+
+def solve_sudoku(puzzle):
+    pass
+
+if __name__ == '__main__':
+    print(solve_sudoku(example_board))
+    
